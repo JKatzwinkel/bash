6d3c666e0e818bd4ab55946d421feb07e338241d
J. Katzwinkel <thor@mintj>
2014-01-10 09:24:00 +0100

    render to printable with html2ps or htmldoc? need to replace all non-ascii chars one by one because neither supports utf8. Or can we convert downloaded html file to iso8859-1?

--- titanic/titanic.sh
+++ titanic/titanic.sh
@@ -3,7 +3,12 @@
 url=$(wget http://www.titanic-magazin.de/ich.war.bei.der.waffen.rss -q -O - \
 	| grep -A 4 "<title>Gärtners kritisches Sonntags" \
 	| sed -n 's/\s*<link>\([^<]*\)<.*/\1/gp')
-echo $url
+
+if [ -n "$(grep $url urls.txt)" ]; then
+	exit
+fi
+
+echo $url >> urls.txt
 
 echo """
 <?xml version=\"1.0\" encoding=\"utf-8\"?>
@@ -15,13 +20,24 @@ echo """
 	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
 </head>
 <body>
-	<div>""" > out.html
+	<div style=\"font-size:5px\">""" > out.html
 
 wget $url -q -O - \
-	| sed -n 's/.*\(Gärtners kritisches Sonntagsfrühstück\):\([^<]*\)<\/div.*/<h3>\1<\/h3><h2>\2<\/h2>/p
+	| sed -n 's/.*\(Gärtners kritisches Sonntagsfrühstück\):\([^<]*\)<\/div.*/<b>\1<\/b><h4>\2<\/h4>/p
 	/<div class=.tt_news-bodytext/,/<\/div>/ { 
 		/<p class/,/^\s*$/ {p}
 	}' \
-	| sed 's/<img[^>]*>//g' >> out.html
+	| sed 's/<img[^>]*>//g' \
+	| sed 's/ä/\&auml;/g' \
+	| sed 's/Ä/\&Auml;/g' \
+	| sed 's/ö/\&ouml;/g' \
+	| sed 's/Ö/\&Ouml;/g' \
+	| sed 's/ü/\&uuml;/g' \
+	| sed 's/Ü/\&Uuml;/g' \
+	| sed 's/ß/\&szlig;/g' >> out.html
 
 echo "</body></html>" >> out.html
+
+#html2ps -o out.ps -e UTF-8 out.html 
+#html2ps -o out.ps out.html 
+htmldoc -t pdf -f out.pdf --webpage out.html
