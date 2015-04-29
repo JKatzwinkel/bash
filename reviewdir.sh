654a1e9da93b772a936a60f90f8326c558929b38
J. Katzwinkel <jkatzwinkel@git.hub>
2015-04-29 10:16:46 +0200

    HDD/portable MP3 player synchro/management tool improved
    if two directories are given as parameters, tracks can be copied to
    second one. Output got nicer. When playback exceeds currently displayed
    track, hitting <Enter> no longer skips next track.

--- reviewdir.sh
+++ reviewdir.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+cpdir=
 if [ $# -lt 1 ]; then
 	echo "Please specify directory to load music from"
 	exit 1
-elif [ ! -d "$1" ]; then
+elif [ ! -d "$(realpath "$1")" ]; then
 	echo "Invalid location $1: not a directory."
 	exit 1
-elif [ $# -gt 2 ]; then
-	if [ -d "$2" ]; then
-		cpdir=$2
+elif [ $# -gt 1 ]; then
+	if [ -d "$(realpath "$2")" ]; then
+		cpdir="$(realpath "$2")"
 		echo "Backup directory set to: $cpdir"
 	else
 		echo "Backup dir $2 invalid."
 	fi
 fi
 
-dir=$1
+dir="$(realpath "$1")"
 
 cmus-remote -C "clear"
 cmus-remote -C "add $dir"
@@ -23,27 +24,57 @@ cmus-remote -p
 
 tmpfile=$(mktemp)
 
+#note: because it returns track currently under cursor,
+# $(cmus-remote -C win-sel-cur 'echo {}') is not as reliable as 'file' field in cmus-remote -Q output
+
+_filename () {
+	cmus-remote -Q > $tmpfile # dump current song info query outputn
+	field=$(grep "^file " "$tmpfile") # retrieve file name of current track
+	echo ${field#file }
+}
+
+latest=
 while true; do
-	cmus-remote -n
-	curfile=$(cmus-remote -C win-sel-cur 'echo {}')
-	cmus-remote -Q > $tmpfile
+	cmus-remote -C "view sorted"
+	[ "$latest" = "$(_filename)" ] && cmus-remote -n
+	curfile=$(_filename)
 	ta=$(sed -n 's/^tag artist \(.*\)$/\1/pg' $tmpfile)
 	tt=$(sed -n 's/^tag title \(.*\)$/\1/pg' $tmpfile)
-	echo $curfile
-	echo "$ta - $tt"
-	echo -n "What to do? [K]eep/[d]elete/[m]odify ID3 tags "
+	# do some fun to look at output stuff
+	twid=$(( $(tput cols)-4 )) # terminal width
+	mp3info="$ta - $tt" # echo some ID3 tags from current MP3
+	fileinfo="$(basename "$curfile")"
+	flocinfo="${curfile%${fileinfo}}"
+	flocinfo="${flocinfo#${dir}}"
+	# determine width of output bounding box:
+	hdw=$(echo -en "70\n${#mp3info}\n${#fileinfo}\n${#flocinfo}" | sort -n | tail -1) # compute minimal width
+	hdw=$(echo $(( $twid < $hdw ? $twid : $hdw ))) # compute max width (terminal width)
+	# begin bounding box
+	echo -n ┌ ;for j in $(seq $hdw); do echo -n "─"; done && echo ──┐
+	echo "│ $mp3info"
+	echo │ $fileinfo
+	echo │ $flocinfo
+	echo -n ├ && for j in $(seq $hdw); do echo -n "─"; done && echo ──┤
+	echo -n "│ > What to do? [K]eep/[d]elete/[m]odify$([ -n "$cpdir" ]&&echo /[c]opy) > "
 	read i
 	if [ "$i" = "d" ]; then
-		echo "removing $curfile"
+		echo "│ > removing $curfile"
 		rm "$curfile"
 	elif [ "$i" = "q" ]; then
 		exit 0
 	elif [ "$i" = "m" ]; then
-		echo -n "Input ID3 tags like this: <Artist> - <Title>: "
+		echo -n "│ > Input ID3 tags like this: <Artist> - <Title>: "
 		read tags
 		ta=$(echo $tags | cut -d- -f 1)
 		tt=$(echo $tags | cut -d- -f 2-)
-		eyeD3 -a "$ta" -t "$tt" "$curfile"
+		eyeD3 -l LEVEL:error -a "$ta" -t "$tt" "$curfile"
+	elif [ "$i" = "c" ]; then
+		if [ -n "$cpdir" ]; then
+			echo "│ > Copying $(basename $curfile)..."
+			cp "$curfile" "$cpdir"
+		fi
 	fi
+	echo -n └; for j in $(seq $hdw); do echo -n "─"; done && echo ──┘
+	latest=$curfile
 done
 	
